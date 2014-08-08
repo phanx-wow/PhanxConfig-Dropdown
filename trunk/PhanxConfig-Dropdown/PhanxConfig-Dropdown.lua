6561c81e178779276c251f3e9d41ea4b8f4f42ab
Phanx-28751 <Phanx-28751@5d13539c-25a5-4bc8-b52a-5474e3ee8778>
2014-08-08 11:02:34 +0000

    - Update README and comment headers for consistency

--- trunk/PhanxConfig-Dropdown/PhanxConfig-Dropdown.lua
+++ trunk/PhanxConfig-Dropdown/PhanxConfig-Dropdown.lua
@@ -4,9 +4,8 @@
 	Based on tekKonfig-Dropdown by Tekkub.
 	Requires LibStub.
 	https://github.com/phanx/PhanxConfigWidgets
-	Copyright 2009-2014 Phanx <addons@phanx.net>
-	Do not redistribute standalone without permisssion.
-	See the accompanying LICENSE file for details.
+	Copyright (c) 2009-2014 Phanx. All rights reserved.
+	See the accompanying README and LICENSE files for more information.
 ----------------------------------------------------------------------]]
 
 local MINOR_VERSION = tonumber(strmatch("$Revision$", "%d+"))
