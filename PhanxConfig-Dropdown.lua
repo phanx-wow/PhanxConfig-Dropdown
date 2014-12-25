6e020c3236515a1179c7f35f949bef0dbfdfb3fe
Phanx <addons@phanx.net>
2014-12-24 22:40:09 -0800

    Smaller comment header

--- PhanxConfig-Dropdown.lua
+++ PhanxConfig-Dropdown.lua
@@ -5,38 +5,13 @@
 	https://github.com/Phanx/PhanxConfig-Dropdown
 
 	Copyright (c) 2009-2014 Phanx <addons@phanx.net>. All rights reserved.
-
-	Permission is granted for anyone to use, read, or otherwise interpret
-	this software for any purpose, without any restrictions.
-
-	Permission is granted for anyone to embed or include this software in
-	another work not derived from this software that makes use of the
-	interface provided by this software for the purpose of creating a
-	package of the work and its required libraries, and to distribute such
-	packages as long as the software is not modified in any way, including
-	by modifying or removing any files.
-
-	Permission is granted for anyone to modify this software or sample from
-	it, and to distribute such modified versions or derivative works as long
-	as neither the names of this software nor its authors are used in the
-	name or title of the work or in any other way that may cause it to be
-	confused with or interfere with the simultaneous use of this software.
-
-	This software may not be distributed standalone or in any other way, in
-	whole or in part, modified or unmodified, without specific prior written
-	permission from the authors of this software.
-
-	The names of this software and/or its authors may not be used to
-	promote or endorse works derived from this software without specific
-	prior written permission from the authors of this software.
-
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-	MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-	IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-	OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-	ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-	OTHER DEALINGS IN THE SOFTWARE.
+	Feel free to include copies of this file WITHOUT CHANGES inside World of
+	Warcraft addons that make use of it as a library, and feel free to use code
+	from this file in other projects as long as you DO NOT use my name or the
+	original name of this library anywhere in your project outside of an optional
+	credits line -- any modified versions must be renamed to avoid conflicts and
+	confusion. If you wish to do something else, or have questions about whether
+	you can do something, email me at the address listed above.
 ----------------------------------------------------------------------]]
 
 local MINOR_VERSION = 20141222
